5a5ef591aa4a352dc21d90faa14acda6fe503d52
Chien Nguyen <100248105+chien-se@users.noreply.github.com>
2026-03-29 01:06:41 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
-const basePath =
-  process.env.NODE_ENV === "production" ? "/Personal_Website" : "";
+const repoName = "Personal_Website";
+
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? `/${repoName}` : "";
 
 const nextConfig: NextConfig = {
   output: "export",
 
   basePath,
-  assetPrefix: basePath, // IMPORTANT for GitHub Pages
+  assetPrefix: basePath,
+
+  trailingSlash: true, // IMPORTANT for GitHub Pages
 
   images: {
     unoptimized: true,
